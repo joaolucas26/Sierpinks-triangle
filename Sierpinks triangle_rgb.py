053260edfd54d34139218ed459314e7d27e5afaa
joao.lucas26 <joao.lucas26@outlook.com>
2023-01-03 17:49:12 -0300

    changing variable names

--- Sierpinks triangle_rgb.py
+++ Sierpinks triangle_rgb.py
@@ -5,11 +5,9 @@ import time
 #from colored import fg
 
 start = time.process_time()
-
-
-def distanc(p1, p2):    
-    return sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
-
+TOTAL_POINTS = 25000
+MAX_VALUE = 255
+MIN_VALUE = 0
 
 #creating canvas
 colormode(255)
@@ -18,34 +16,33 @@ screen.setup(1050,1050)
 screen.title('Sierpinks triangle')
 screen.bgcolor((0,0,0))
 
-
-
-
-
 #creating the triangle
-
-height = 500
+HEIGHT = 500
 #max positions
-red = (0, height) 
-green = (-height, -height)
-blue = (height, -height)
+RED = (0, HEIGHT) 
+GREEN = (-HEIGHT, -HEIGHT)
+BLUE = (HEIGHT, -HEIGHT)
+
+
+def distanc(p1, p2):    
+    return sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
 
 #iniciando aleatoriamente um ponto dentro do triangulo
-turtle_pos = (random.randint(-150, 150), random.randint(-150, 150))
+TURTLE_POS = (random.randint(-150, 150), random.randint(-150, 150))
 
-for i in range(25000):
+for i in range(TOTAL_POINTS):
     dot_turtle = Turtle()
     dot_turtle.hideturtle()  
     dot_turtle.penup()
     dot_turtle.speed(0)
-    dot_turtle.goto(turtle_pos)     
+    dot_turtle.goto(TURTLE_POS)     
 
-    point = random.sample([red, green, blue],1)
+    point = random.sample([RED, GREEN, BLUE],1)
 
-    dist_red = distanc(turtle_pos, red)
-    dist_green = distanc(turtle_pos, green)
-    dist_blue = distanc(turtle_pos, blue)
+    dist_red = distanc(TURTLE_POS, RED)
+    dist_green = distanc(TURTLE_POS, GREEN)
+    dist_blue = distanc(TURTLE_POS, BLUE)
 
 
     min_ = min(dist_red, dist_blue, dist_green)
@@ -55,37 +52,37 @@ for i in range(25000):
     b = 0
 
     if dist_red == min_:
-        r = 255        
+        r = MAX_VALUE        
 
     else:
-        r_ = 2 * height - dist_red
-        r = height * r_ / (r_ +height)
+        r_ = 2 * HEIGHT - dist_red
+        r = HEIGHT * r_ / (r_ +HEIGHT)
 
     if dist_green == min_:
-        g = 255
+        g = MAX_VALUE
 
     else:
-        g_ = 2 * height - dist_green
-        g = height * g_ / (g_ +height)
+        g_ = 2 * HEIGHT - dist_green
+        g = HEIGHT * g_ / (g_ +HEIGHT)
   
 
     if dist_blue == min_:
-        b = 255
+        b = MAX_VALUE
 
     else:
-        b_ = 2 * height - dist_blue
-        b = height * b_ / (b_ +height)
+        b_ = 2 * HEIGHT - dist_blue
+        b = HEIGHT * b_ / (b_ +HEIGHT)
         
-    turtle_pos = ((point[0][0] + turtle_pos[0])/2, (point[0][1] + turtle_pos[1])/2)
+    TURTLE_POS = ((point[0][0] + TURTLE_POS[0])/2, (point[0][1] + TURTLE_POS[1])/2)
 
     r = int(abs(r))
     g = int(abs(g))
     b = int(abs(b))
 
-    print ( ('white')+ str(i) + " / 25000 ", ('red') + str(r), ('green') + str(g), ('blue') + str(b))
+    print (f'{i}/{TOTAL_POINTS} - COLOR: ({r},{g}. {b}) - POINT: X:{round(TURTLE_POS[0], 2)}, Y:{round(TURTLE_POS[1], 2)}')
 
     #if you have colored lib installed
-    #print ( fg('white')+ str(i) + " / 25000 ", fg('red') + str(r), fg('green') + str(g), fg('blue') + str(b))
+    #print ( fg('white') + str(i) + " / 25000 ", fg('red') + str(r), fg('green') + str(g), fg('blue') + str(b))
 
     dot_turtle.pendown()
     dot_turtle.dot(3.5, (r,g,b))
